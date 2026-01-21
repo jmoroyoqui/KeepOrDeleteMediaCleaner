e92350afd59c8cac6d3d6d1c48a0eb6550ff5a90
jmoroyoqui <moroyoquigil.julio@gmail.com>
2026-01-20 22:30:56 -0700

    Add project files.

---
+++ KeepOrDeleteMediaCleaner/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace KeepOrDeleteMediaCleaner
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ KeepOrDeleteMediaCleaner/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KeepOrDeleteMediaCleaner
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
